3f098bb27d2f7dde250413c4703e0a768b0f2a6e
gitproject23 <n.sreenu23@gmail.com>
2023-10-21 23:37:25 +0530

    first commit.

---
+++ src/main/java/com/soft/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.soft.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class EmployeeController {
+@GetMapping("/cnu")
+
+public String example(Model model) {
+    model.addAttribute("message", "Hello, World!");
+    System.out.println("Hello index");
+    String htmlContent = "<html><body><h1>Hello, World!</h1></body></html>";
+    return htmlContent;
+
+}
+
+    @GetMapping("/cnuu")
+    public String saveEmployees(){
+        System.out.println("Hello");
+
+        return "hello sreenu";
+    }
+}
